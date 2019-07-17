c0f60c504a5b6108d273191be278bc7ad61bfdaa
josh <josh@pepabo.com>
2019-07-17 19:19:22 +0900

    Initial Commit

---
+++ Pokemon/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Pokemon
+//
+//  Created by josh on 2019/07/17.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
